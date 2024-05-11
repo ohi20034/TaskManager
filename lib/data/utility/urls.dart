13e2bf3a8a5905ba5c26af179dc8980ed1d2cc3a
Imdadul Haque Ohi <imdadul0202@gmail.com>
2024-05-11 08:48:07 +0600

    update

--- lib/data/utility/urls.dart
+++ lib/data/utility/urls.dart
@@ -1,4 +1,4 @@
 class Urls {
-  static final String _baseUrl = 'https://task.teamrabbil.com/api/v1';
-  static final String registration = '$_baseUrl/registration';
+  static const String _baseUrl = 'https://task.teamrabbil.com/api/v1';
+  static const String registration = '$_baseUrl/registration';
 }
